20ed8a9cb8e5bbd7fa26d47e2d62711a6a3cc15e
rurara <rurara@ruraraui-MacBook-Pro.local>
2013-07-28 11:50:21 +0900

    - 폰트 스타일 별로 출력되도록 업데이트

--- SampleFont/ViewController.h
+++ SampleFont/ViewController.h
@@ -13,6 +13,7 @@
 
 @interface ViewController : UIViewController<UITableViewDataSource, UITableViewDelegate>{
     UITableView *fontTableView;
+    NSMutableArray *fontArray;
 }
 
 @end
--- SampleFont/ViewController.m
+++ SampleFont/ViewController.m
@@ -16,39 +16,38 @@
     fontTableView = [[UITableView alloc]initWithFrame:CGRectMake(0, 0, 320, 460) style:UITableViewStylePlain];
     
     fontTableView.dataSource = self;
-    fontTableView.delegate = self;
-    
-    NSArray* tempFonts = [UIFont familyNames];
-    for(NSString* aFont in tempFonts){
-        NSLog(@"%@",aFont);
-    }
-    
+    fontTableView.delegate = self;    
     [self.view addSubview:fontTableView];
+
+    fontArray = [NSMutableArray new];
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
 
 -(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
-    NSArray* tempFonts = [UIFont familyNames];
-    return [tempFonts count];
     
+    for (NSString *fontName in [UIFont familyNames]) {
+        for (NSString *fontStyleName in [UIFont fontNamesForFamilyName:fontName]) {
+            [fontArray addObject:fontStyleName];
+            NSLog(@"%@",fontStyleName);
+        }
+    }
+
+    return [fontArray count];
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"testcell"];
     
-    NSArray* tempFonts = [UIFont familyNames];
-    
     if (cell==nil) {
         cell = [[UITableViewCell alloc] initWithFrame:CGRectZero];
-        NSString *tempString = [tempFonts objectAtIndex:indexPath.row];
-        
-        cell.textLabel.text = [NSString stringWithFormat:@"%@/鸡肉/とりにく/チキン/치킨", tempString];
-        [cell.textLabel setFont:[UIFont fontWithName:tempString size:10]];
+        NSString *fontStyleName = [fontArray objectAtIndex:indexPath.row];
+
+        cell.textLabel.text = [NSString stringWithFormat:@"%@/鸡肉/とりにく/チキン/치킨", fontStyleName];
+        [cell.textLabel setFont:[UIFont fontWithName:fontStyleName size:10]];
     }
 
     return cell;
-    
 }
 
 @end
